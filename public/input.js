efcedd7ed99fa2f37423fa79842072a5b9f359f2
Daniel Kern <37657554+kerndand@users.noreply.github.com>
2020-01-08 14:35:19 +0100

    Add files via upload

--- public/input.js
+++ public/input.js
@@ -6,12 +6,16 @@ SOCKET.on("data", (data) => {
   gyro = data;
 
 
- document.getElementsByClassName('cube')[0].style.transform = 'rotateX(' + gyro.x +'deg) rotateY(' + gyro.y + 'deg) rotateZ(' + gyro.z + 'deg)';
-});
+     document.getElementsByClassName('cube')[0].style.transform = 'rotateX(' + gyro.x + 'deg) rotateY(' + gyro.y + 'deg) rotateZ(' + gyro.z + 'deg)';
+    //  document.getElementsByClassName('cube')[1].style.transform = 'rotateX(' + gyro.x + 'deg) rotateY(' + gyro.y + 'deg) rotateZ(' + gyro.z + 'deg)';
+    //  document.getElementsByClassName('cube')[2].style.transform = 'rotateX(' + gyro.x + 'deg) rotateY(' + gyro.y + 'deg) rotateZ(' + gyro.z + 'deg)';
+    //  document.getElementsByClassName('cube')[3].style.transform = 'rotateX(' + gyro.x + 'deg) rotateY(' + gyro.y + 'deg) rotateZ(' + gyro.z + 'deg)';
+  });
+
 
 // window.addEventListener("deviceorientation", (e) => {
   // const beta = e.beta;
   // const gamma = e.gamma;
-  
+
 // document.getElementsByClassName('cube')[0].style.transform = 'rotateY(' + gamma*3 + 'deg) rotateX(' + beta*3 +'deg)';
 // }, true);
