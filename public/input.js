f9f176c1e3e993c33dd571826ce9b0c8b377bea9
Daniel Kern <37657554+kerndand@users.noreply.github.com>
2020-01-08 10:59:41 +0100

    Add files via upload

--- public/input.js
+++ public/input.js
@@ -1,6 +1,17 @@
 const SOCKET = io.connect("http://localhost:3000");
-var gyrox;
+let gyro;
 
 SOCKET.on("data", (data) => {
   console.log(data);
+  gyro = data;
+
+
+ document.getElementsByClassName('cube')[0].style.transform = 'rotateY(' + gyro.y + 'deg) rotateX(' + gyro.x +'deg)';
 });
+
+// window.addEventListener("deviceorientation", (e) => {
+  // const beta = e.beta;
+  // const gamma = e.gamma;
+  
+// document.getElementsByClassName('cube')[0].style.transform = 'rotateY(' + gamma*3 + 'deg) rotateX(' + beta*3 +'deg)';
+// }, true);
